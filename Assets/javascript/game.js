3a9f1df26a85a079d4d22412b898eea224f45f58
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-02-10 20:46:06 -0500

    working out the js for game functionality, adding css style

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,37 +1,65 @@
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var words = ["HAPPY", "KITTY", "HUNGRY", "TUNA", "FUZZY", "CRAZY", "WEIRD", "MEOW", "FLOOF"];
+var words = ["HAPPY", "KITTY", "HUNGRY", "TUNA", "FUZZY", "CRAZY", "SIAMESE", "MAINE COON", "BURMESE", "TABBY", "TUXEDO", "MEOW", "FLOOF", "WHISKERS", "PURR", "PAWS"];
 //var guess = event.key;
 var guesses = [];
+var wins = [];
 
-var wordNum = Math.round((Math.random() * words.length) + 1); 
+// Picking a random word from the words array
+var wordNum = Math.floor(Math.random() * words.length); 
 var word = words[wordNum];
-var spaces = document.getElementById("wordspace");
-var letterbox = document.getElementById("guesses");
-
-var setUpWord = function (word) {
-	word.toUpperCase();
-	spaces.toUpperCase();
-}
+//var spaces = document.getElementById("wordspace");
 
 
-var guess = function feedLetters() {
-	document.onkeypress = function(event) {
-		spaces.textContent = event.key;
-	}
+
+var wordspace = document.getElementById("wordspace");
+
+function makeWord() {
+	document.getElementById("wordspace").innerHTML = word;
 };
+//wordspace.onkeypress = 
+
+//document.onkeypress = makeWord();
+
+document.getElementById("wordspace").addEventListener("keypress", makeWord, false);
 
 function guessedLetters() {
 	for (var i = 0; i < guess.length; i++) {
-			(guesses.push(guess) + (i + 1));
+		(guesses.push(guess) + (i + 1));
+	}
+	guesses.innerHTML.toUpperCase();
+};
+
+ //makeWord();
+
+document.getElementById("guesses").innerHTML = guesses;
+
+
+
+
+
+
+
+
+
+/*
+	setUpWord: function(word) {
+		word.toUpperCase();
+		spaces.toUpperCase();
+	}
+
+	guess: function() {
+		document.onkeypress = function(event) {
+			spaces.textContent = event.key;
 		}
-		letterbox.innerHTML.toUpperCase();
-}
+	};
 
 
 
-//for (i = 0; i < word.length; i++) {
+// guessedLets.includes(word.charAt[i])
+// join()
+
+for (i = 0; i < word.length; i++) {
 	spaces.innerHTML = spaces;
+	word.charAt[i];
 
-// document.create.element("div");
-
-
+document.create.element("div"); */
