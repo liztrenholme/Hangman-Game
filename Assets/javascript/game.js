27ce57e547d635ea0631485224054a02f669ec66
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-02-11 22:23:08 -0500

    ughhhhhh

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,45 +1,66 @@
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var words = ["HAPPY", "KITTY", "TREATS", "TUNA", "FUZZY", "CRAZY", "SIAMESE", "MAINE COON", "BURMESE", "TABBY", "TUXEDO", "MEOW", "FLOOF", "WHISKERS", "PURR", "PAWS"];
+var words = ["HAPPY", "KITTY", "TREATS", "TUNA", "FUZZY", "CRAZY", "SIAMESE", "MEESERS", "BURMESE", "TABBY", "TUXEDO", "MEOW", "FLOOF", "WHISKERS", "PURR", "PAWS"];
+var guessesLeft = 15;
+var wins = 0;
+var spaces = [" ", "_", "_ _", "_ _ _", "_ _ _ _", "_ _ _ _ _", "_ _ _ _ _ _", "_ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _"];
+var letter = document.addEventListener("keyup", guessTheLetter, [false]);
 var guesses = [];
-var wins = [];
-var spaces = ["_ ", "_ _", "_ _ _", "_ _ _ _", "_ _ _ _ _", "_ _ _ _ _ _", "_ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _"];
 
 // Picking a random word from the words array
 var wordNum = Math.floor(Math.random() * words.length); 
 var word = words[wordNum];
 
+function beginGame() {
+	document.addEventListener("keyup", function() {
+		makeWord();
+		spaceOut();
+	}, false);
+}
 
-
-// function for putting word onto board
+// putting word onto board
 function makeWord() {
 	document.getElementById("wordspace").innerHTML = word;
+	console.log(word);
 };
 
+// putting spaces in place of letters
 function spaceOut() {
-	//for (i = 0; i < word.length; i++) {
-	//spaces.innerHTML = spaces[word.length];
-	//}
 	document.getElementById("wordspace").innerHTML = spaces[word.length];
 };
 
-function guessedLetters() {
-	for (var i = 0; i < guesses.length; i++) {
-		(guesses.push(guesses) + (i + 1));
+// 
+function guessTheLetter() {
+	for (var i = 0; i < alphabet.length; i++) {
+		if (letter === word.charAt()) {
+			for (var i = 0; i < word.length; i++) {
+				if (word[i] === letter) {
+					word[i] = letter;
+				}
+			}
+		}
+		else if (word.indexOf(letter) === -1) { // letter is NOT in the word
+    guesses.push(letter); // update letters guessed
+    guesses.innerHTML = guesses.join(', ');
+		} 
 	}
-	//guesses.toUpperCase();
-	document.getElementById("guesses").innerHTML = guesses[i];
-};
+}
+
+for (var i = 0; i < guesses.length; i++) {
+	letter.toUpperCase();
+	(letter.push(guesses) + (i + 1));
+}
+//document.getElementById("guesses").innerHTML = letter;
+console.log(letter);
+//};
 
 // Begins game when user presses any key
-document.addEventListener("keyup", function() {
-	makeWord();
-	spaceOut();
-}, false);
+beginGame();
 
 // Lists each guessed letter
-document.addEventListener("keyup", function() {
-	guessedLetters();
-}, false);
+//var letter = document.addEventListener("keyup", function() {
+
+//	guessedTheLetter();
+//}, false);
 
 
 
@@ -68,9 +89,5 @@ document.addEventListener("keyup", function() {
 
 // guessedLets.includes(word.charAt[i])
 // join()
-
-for (i = 0; i < word.length; i++) {
-	spaces.innerHTML = spaces;
-	word.charAt[i];
 
 document.create.element("div"); */
