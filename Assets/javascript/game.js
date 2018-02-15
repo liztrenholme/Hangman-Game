b27197d7c44ee39a6b1b2b68b0eba11f84cb53ac
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-02-15 10:08:43 -0500

    rearranged and cleaned up some things, some lost game functionality is now working

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -2,11 +2,11 @@ var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 var words = ["HAPPY", "KITTY", "TREATS", "TUNA", "FUZZY", "CRAZY", "SIAMESE", "MEESERS", "BURMESE", "TABBY", "TUXEDO", "MEOW", "FLOOF", "WHISKERS", "PURR", "PAWS"];
 var guessesLeft = 15;
 var wins = 0;
-var spaces = [" ", "_", "_ _", "_ _ _", "_ _ _ _", "_ _ _ _ _", "_ _ _ _ _ _", "_ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _"];
 var guesses = [];
 var incorrectLetters = [];
 var letter;
 var spacedWord;
+var lettersInWord = [];
 
 // Picking a random word from the words array
 var wordNum = Math.floor(Math.random() * words.length); 
@@ -14,49 +14,36 @@ var word = words[wordNum];
 
 function beginGame() {
 	document.addEventListener("keyup", function() {
-		makeWord();
-		spaceOut();
+
 	}, false);
 };
 
 // putting word onto board	
 function makeWord() {
-	document.getElementById("wordspace").textContent = word;
+	
+	for (var i = 0; i < word.length; i++) {
+		lettersInWord.push("_");
+	}
+	document.getElementById("wordspace").textContent = lettersInWord.join(" ");
 	console.log(word);
 };
-
-// putting spaces in place of letters
-function spaceOut() {
-	document.getElementById("wordspace").textContent = spaces[word.length];
-};
-
+		makeWord();
 
 document.onkeyup = function(event) {
 	letter = event.key;
 	letter = letter.toUpperCase();
-//	console.log(letter);
+	console.log(letter);
 	console.log(word);
-	spacedWord = spaces[word.length];
-	console.log(spacedWord);
 
-// letters not being validated?
-	for (i = 0; i < letter.length; i++) {
-		if (letter === alphabet[i]) {
-			letter = letter;
-		}
-		else {
-			//letter = "blah";
-			console.log(letter);
-		}
-	}
-console.log(letter);
+	// need some way to validate letter to exclude repeat guesses and non letter keys
+	console.log(letter);
 	if (word.includes(letter)) { 
 		for (var i = 0; i < word.length; i++) {
-		if (word.charAt(i) === letter) {
-			spacedWord.slice(i, i + 1);
+			if (word.charAt(i) === letter) {
+				lettersInWord.slice(i, i + 1);
+			}
 		}
 	}
-}
 	else {
 		guessesLeft--;
 		document.getElementById("guesses-left").textContent = guessesLeft;
@@ -64,6 +51,16 @@ console.log(letter);
 		document.getElementById("guesses").textContent = guesses + " ";
 
 	};
-}
 
+	if (guessesLeft === 0) {
+		alert("You lost this round!");
+		beginGame();
+	}
+	else if (lettersInWord.indexOf("_") === -1) {
+		alert("You won this round!");
+		makeWord();
+		wins++;
+		document.getElementById("wins").textContent = wins;
+	}
+}
 beginGame();
