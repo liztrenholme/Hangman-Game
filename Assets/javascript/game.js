7be2b1ee2b4ed1f1b1f4258aef991fb7a8b1ca98
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-10-08 11:38:52 -0400

    styling

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,5 +1,5 @@
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var words = ["HAPPY", "KITTY", "TREATS", "TUNA", "FUZZY", "CRAZY", "SIAMESE", "MEESERS", "BURMESE", "TABBY", "TUXEDO", "MEOW", "FLOOF", "WHISKERS", "PURR", "PAWS"];
+var words = ["HAPPY", "KITTY", "TREATS", "TUNA", "FUZZY", "CRAZY", "SIAMESE", "MEESERS", "BURMESE", "TABBY", "TUXEDO", "MEOW", "FLOOF", "WHISKERS", "PURR", "PAWS", "CLOWDER", "HIMALAYAN", "PERSIAN", "CUDDLY", "SHORTHAIR", "LONGHAIR"];
 var guessesLeft = 15;
 var wins = 0;
 var guesses = [];
